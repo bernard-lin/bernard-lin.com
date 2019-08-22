08584ead8878ed28828ccf29e092675b53416cc7
Bernard Lin <bernardlin12@gmail.com>
2019-08-22 17:03:15 -0400

    linter changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,29 +1,36 @@
 module.exports = {
-    siteMetadata: {
-        title: 'Bernard Lin',
-        description: 'Personal website'
-
+  siteMetadata: {
+    title: "Bernard Lin",
+    description: "Personal website"
+  },
+  plugins: [
+    "gatsby-plugin-emotion",
+    "gatsby-plugin-react-helmet",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-plugin-mdx",
+      options: {
+        defaultLayouts: {
+          default: require.resolve("./src/components/layout.jsx")
+        },
+        gatsbyRemarkPlugins: [{ resolve: "gatsby-remark-images" }],
+        plugins: [{ resolve: "gatsby-remark-images" }]
+      }
     },
-    plugins: ['gatsby-plugin-emotion', 'gatsby-plugin-react-helmet', 'gatsby-transformer-sharp', 'gatsby-plugin-sharp', {
-        resolve: 'gatsby-plugin-mdx',
-        options: {
-            defaultLayouts: {
-                default: require.resolve('./src/components/layout.jsx')
-            },
-            gatsbyRemarkPlugins: [{resolve: 'gatsby-remark-images'}],
-            plugins: [{resolve: 'gatsby-remark-images'}]
-        }
-    }, {
-        resolve: 'gatsby-source-filesystem',
-        options: {
-            name: 'posts',
-            path: 'posts'
-        }
-    }, {
-        resolve: 'gatsby-source-filesystem',
-        options: {
-            name: 'images',
-            path: 'images'
-        }
-    }]
-}
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "posts",
+        path: "posts"
+      }
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "images"
+      }
+    }
+  ]
+};
